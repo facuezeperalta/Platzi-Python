7424692932d64df8900ef6b3d869bb9907564efb
Facundo Peralta <facundoperaltaezequiel@gmail.com>
2023-04-20 16:19:13 -0300

    empezamos el repaso

---
+++ strings.py
@@ -0,0 +1,13 @@
+name = 'Facundo'
+last_name = 'Peralta'
+
+print (name)
+print (last_name)
+
+
+full_name = print(name + ' ' + last_name)
+
+
+template = "Hola me llamo {}".format(full_name)
+
+print (template)
