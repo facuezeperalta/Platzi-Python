5aa7c388a1530f44790c23a50b75ec5517c5948f
Facundo Peralta <facundoperaltaezequiel@gmail.com>
2023-04-20 18:06:25 -0300

    if

---
+++ 13-if.py
@@ -0,0 +1,22 @@
+if True: 
+    print ('deberia ejecutarse')
+
+if False:
+    print ('Nunca deberia salir')
+
+"""     
+pet = input('Cual es tu mascota favorita?')
+
+if pet == 'perro':
+    print ('Genial! los perros son compañeros y juguetones')
+if pet == 'gato':
+    print ('los gatos son buena compañía pero mas independientes') """
+""" 
+stock = int(input('Digita el stock=> '))
+
+if stock >= 100 and stock <=1000:
+    print ('El stock está bien') """
+
+
+
+    
